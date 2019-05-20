ae2b559b359120810aed1d63fbebd40fae6f2548
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-05-20 22:23:52 +0530

    Create check number.c

---
+++ check number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("The number is positive");
+    }
+    else if(n<0)
+    {
+        printf("The number is negative");
+    }
+    else(n==0)
+    {
+    printf("The number is Zero");
+    }
+
+    return 0;
+}
