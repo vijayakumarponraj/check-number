e8ac8336ead7e75ed81f3ed2867f774bb49352ef
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-05-22 17:47:51 +0530

    Create largest number.c

---
+++ largest number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b & a>c)
+    {
+        printf("a is big");
+    }
+    else if(b>a & b>c)
+    {
+        printf("b is big");
+    }
+    else if(c>a & c>b)
+    {
+        printf("c is big");
+    }
+    return 0;
+
+}
