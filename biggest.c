4ebf0ab6906e518f188e38c932e8f4e76a95d4d6
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-05-22 18:03:55 +0530

    Update biggest.c

--- biggest.c
+++ biggest.c
@@ -1,20 +1,13 @@
-#include<stdio.h>
-int main()
-{
-    int a,b,c;
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>b & a>c)
-    {
-        printf("a is big");
-    }
-    else if(b>a & b>c)
-    {
-        printf("b is big");
-    }
-    else if(c>a & c>b)
-    {
-        printf("c is big");
-    }
-    return 0;
+#include <stdio.h>
 
+int main(void) {
+		int a,b,c;
+		scanf("%d%d%d",&a,&b,&c);
+		if(a>b&&a>c)
+		printf("%d",a);
+		else if(b>a&&b>c)
+		printf("%d",b);
+		else
+		printf("%d",c);
+	return 0;
 }
