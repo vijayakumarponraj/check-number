31c0492c157a48883a7add0aba36acaf9000090d
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-05-23 11:42:13 +0530

    digitornot.c

---
+++ digitornot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    void main()
+    {
+    char n;
+    scanf("%s",&n);
+    if(isdigit(n))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    }
