2db92ccf55d72402e3ab57790fe351753be85ecf
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-05-23 10:09:56 +0530

    printmin.c

---
+++ printmin.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,a[100],ans;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        ans=a[0];
+    }
+    for(i=1;i<n;i++)
+    if(ans>a[i])
+    {
+        ans=a[i];
+    }
+    printf("%d ",ans);
+    
+}
