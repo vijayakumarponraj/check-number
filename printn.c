cbcf536fcf2ea707844e6f5f5f2b64107395111f
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-05-31 13:12:42 +0530

    printn.c

---
+++ printn.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+ int n,i;
+ char s[30];
+ scanf("%s%d",&s,&n);
+    for(i=1;i<=n;i++)
+    {
+    
+        printf("%s\n",s);
+    }
+
+    return 0;
+}
