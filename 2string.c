0ceab3ebd1ed14f86ffd8e104ec5f906e8400b8e
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-06-01 00:30:33 +0530

    2string.c

---
+++ 2string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  	char Str1[100], Str2[100];
+  	int result, i;
+ 	i = 0;
+  	gets(Str1);
+  	gets(Str2);
+  	while(Str1[i] == Str2[i] && Str1[i] == '\0')
+	  	i++;
+		   
+  	if(Str1[i] < Str2[i])
+   	{
+   		printf("\n%s",Str2);
+	}
+	else if(Str1[i] > Str2[i])
+   	{
+   		printf("\n %s",Str2);
+	}
+	else
+   	{
+   		printf("\n Equal");
+	}
+  	
+  	return 0;
+}
