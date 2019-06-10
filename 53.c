eee19ff5edfd0e5e0352f3abbe4c6e96e21800f1
vijayakumarponraj <50840993+vijayakumarponraj@users.noreply.github.com>
2019-06-10 20:20:24 +0530

    53.c

---
+++ 53.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,sum;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        sum=sum+i;
+      }
+       printf("%d\n",sum);
+    return 0;
+
+}
